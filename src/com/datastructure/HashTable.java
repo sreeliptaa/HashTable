d0fb4c90939914dabeb6002d018465d79a6c4d0e
sreeliptaa <sreelipta65@gmail.com>
2021-08-25 23:35:17 +0530

    Added: finding frequency of word in sentence

--- src/com/datastructure/HashTable.java
+++ src/com/datastructure/HashTable.java
@@ -1,9 +1,29 @@
 package com.datastructure;
 
-import javax.sound.midi.Soundbank;
-
+/**
+ * Purpose - Find frequency of words in a sentence - To be or not to be
+ * @author - Sreelipta
+ */
 public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome to Hashtable Programs");
+        //Creating object as hashMap for MyHashTable class
+        MyHashTable<String, Integer> hashMap = new MyHashTable();
+        String statement = "To be or not to be";
+        //converting statement into lowercase 
+        String[] statementArray = statement.toLowerCase().split(" ");
+
+        // Iterating over the array.
+        for (String word : statementArray) {
+            Integer count = hashMap.get(word);
+
+            if (count == null)
+                count = 1;
+            else
+                count = count + 1;
+            hashMap.add(word, count);
+        }
+        System.out.println(hashMap);
     }
 }
+
