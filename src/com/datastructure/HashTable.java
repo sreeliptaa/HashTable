60a00973e7ad20f7393ef75504b44d01b1f65585
sreeliptaa <sreelipta65@gmail.com>
2021-08-25 21:39:29 +0530

    Added:welcome message

---
+++ src/com/datastructure/HashTable.java
@@ -0,0 +1,9 @@
+package com.datastructure;
+
+import javax.sound.midi.Soundbank;
+
+public class HashTable {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hashtable Programs");
+    }
+}
